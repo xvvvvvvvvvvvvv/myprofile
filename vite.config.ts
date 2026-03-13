bcf69314d6e86af66c4b6a2d3e9b367b27eb381f
xvvvvvvvvvvvvv <137134804+xvvvvvvvvvvvvv@users.noreply.github.com>
2026-03-13 20:21:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-// 🌟 1. 引入压缩插件
-import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    // 🌟 2. 启用 Gzip 压缩插件
-    viteCompression({
-      verbose: true,     // 打包时在终端输出压缩结果
-      disable: false,
-      threshold: 10240,  // 只处理体积大于 10KB 的文件
-      algorithm: 'gzip', // 使用 gzip 压缩
-      ext: '.gz',        // 生成 .gz 文件
-    })
+    // 🌟 压缩插件已经彻底删除了，交给 Vercel 自动处理！
   ],
   resolve: {
     alias: {
